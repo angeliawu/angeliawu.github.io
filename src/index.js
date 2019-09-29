7a74b0cd4ec26ce3997a4b617fdc1afb1891b7bb
jat4625 <jatorr98@gmail.com>
2019-09-29 15:22:44 -0500

    refactored filesystem

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
-import Scene0 from './scenes/Scene0.js';
+import Sprint1 from './scenes/sprint1.js';
 import GameOverScene from './scenes/GameOverScene.js';
 import WinScene from './scenes/WinScene.js';
-
 import Config from './config/config.js';
-
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
-    this.scene.add('Scene0', Scene0);
+    this.scene.add('Sprint1', Sprint1);
     this.scene.add('GameOverScene', GameOverScene);
     this.scene.add('WinScene',WinScene);
 
--- src/scenes/sprint1.js
+++ src/scenes/sprint1.js
@@ -1,9 +1,9 @@
 /*global Phaser*/
 import * as ChangeScene from './InGameChangeScene.js'
 
-export default class Scene0 extends Phaser.Scene {
+export default class Sprint1 extends Phaser.Scene {
   constructor () {
-    super('Scene0');
+    super('Sprint1');
   }
 
   init (data) {
@@ -12,9 +12,8 @@ export default class Scene0 extends Phaser.Scene {
 
   preload () {
     // Preload assets
-    this.load.image("tiles", "./assets//tilemaps/newTileset.png");
-    this.load.tilemapTiledJSON("map", "./assets/tilesets/tuxemon-town.json");
-    this.load.atlas("atlas","./assets/atlas/atlas.png","./assets/atlas/atlas.json");
+    this.load.image("tiles", "./assets/tilemaps/newTileset.png");
+    this.load.tilemapTiledJSON("map", "./assets/tilemaps/friedOrFlight.json");
     this.load.image("crate", "./assets/crate.png");
     this.load.image("Lcrate", "./assets/Lcrate.png");
     this.load.audio("theme","./assets/InGame.wav");
@@ -47,24 +46,21 @@ export default class Scene0 extends Phaser.Scene {
   create() {
     //Add change scene event listeners
     ChangeScene.addSceneEventListeners(this)
-
-    //load map
-    const map = this.make.tilemap({ key: "map"});
-
-    const tileset = map.addTilesetImage("newTileset", "tiles");
-    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
-
-    this.gameWin = false;
-    this.gameLose = false;
-
     //add music
-    this.music=this.sound.add('theme');
+    this.music= this.sound.add('theme');
     this.music.play({
       volume:.3,
       loop:true
     });
+
+    //load map
+    this.gameWin = false;
+    this.gameLose = false;
+    const map = this.make.tilemap({ key: "map"});
+    const tileset = map.addTilesetImage("newTileset", "tiles");
+    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
 
     //create game timer
     this.initialTime = 30
@@ -94,8 +90,6 @@ export default class Scene0 extends Phaser.Scene {
         this.gameLose = true;
       }
     }
-
-    //worldLayer.setCollisionBetween(12, 44);
     worldLayer.setCollisionByProperty({ collides: true});
     //aboveLayer.setDepth(10);
     const spawnPoint = map.findObject(
@@ -103,227 +97,126 @@ export default class Scene0 extends Phaser.Scene {
       obj => obj.name === "Spawn Point"
     );
 
-  //player attributes
-  this.player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "Potato");
+    //player attributes
+    this.player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "Potato");
 
-  this.physics.add.collider(this.player, worldLayer);
-  this.player.body.setSize(32,48,32,32);
-  this.player.body.width = 26;
+    this.physics.add.collider(this.player, worldLayer);
+    this.player.body.setSize(32,48,32,32);
+    this.player.body.width = 26;
 
-  this.cursors = this.input.keyboard.createCursorKeys();
-  const camera = this.cameras.main;
-  camera.startFollow(this.player);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    const camera = this.cameras.main;
+    camera.startFollow(this.player);
 
-  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
-  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  this.anims.create({
-      key: "walk",
-      frames: this.anims.generateFrameNumbers('Potato', { start: 0, end: 5}),
-      frameRate: 10,
-      repeat: -1
+    // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.anims.create({
+        key: "walk",
+        frames: this.anims.generateFrameNumbers('Potato', { start: 0, end: 5}),
+        frameRate: 10,
+        repeat: -1
+      });
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('Potato', { start: 0, end: 0}),
+        frameRate: 10,
+        repeat: -1
     });
-  this.anims.create({
-      key: 'idle',
-      frames: this.anims.generateFrameNumbers('Potato', { start: 0, end: 0}),
-      frameRate: 10,
-      repeat: -1
-  });
 
 
-  //win condition
-  var win = map.createFromObjects('Objects','winPoint', {key: 'exit'});
-  this.winGroup = this.physics.add.group();
-  this.winGroup.children.iterate(function(child) {
-    child.setImmoveable(true);
-    child.refreshBody();
-  });
-  this.physics.add.collider(this.winGroup, worldLayer, function(s1){
-    var b1 = s1.body;
-    b1.stop();
-  });
-  this.physics.add.collider(this.player, this.winGroup, this.endScene, null, this);
-
-  for(var i = 0; i < win.length; i++){
-    this.winGroup.add(win[i]);
-  }
-
-  //Crate attributes
-  var crate = map.createFromObjects('Objects','cratePoint', {key: 'crate'});
-  this.crateGroup = this.physics.add.group();
-  this.crateGroup.children.iterate(function(child) {
-    child.setImmoveable(false);
-    child.refreshBody();
-  });
-  this.physics.add.collider(this.crateGroup, worldLayer, function(s1){
-    var b1 = s1.body;
-    b1.stop();
-  });
-  this.physics.add.collider(this.player, this.crateGroup);
-  this.physics.add.collider(this.crateGroup,this.crateGroup);
-
-  for(var i = 0; i < crate.length; i++){
-    this.crateGroup.add(crate[i]);
-    crate[i]
-    .body
-    .CollideWorldBounds = true;
-    crate[i]
-    .body.bounce.set(0.1);
-    crate[i]
-    .body.setDrag(100);
-  }
-  //Large Crate attributes
-  var Lcrate = map.createFromObjects('Objects','LCratePoint', {key: 'Lcrate'});
-  this.LcrateGroup = this.physics.add.group();
-  this.LcrateGroup.children.iterate(function(child) {
-    child.setImmoveable(false);
-    child.refreshBody();
-  });
-  this.physics.add.collider(this.LcrateGroup, worldLayer, function(s1){
-    var b1 = s1.body;
-    b1.stop();
-  });
-  this.physics.add.collider(this.player, this.LcrateGroup);
-  this.physics.add.collider(this.crateGroup,this.LcrateGroup);
-  this.physics.add.collider(this.LcrateGroup);
-
-  for(var i = 0; i < Lcrate.length; i++){
-    this.LcrateGroup.add(Lcrate[i]);
-    Lcrate[i]
-    .body
-    .CollideWorldBounds = true;
-    Lcrate[i]
-    .body.bounce.set(0.1);
-    Lcrate[i]
-    .body.setDrag(100);
-    if (Lcrate[i].angle == 90 || Lcrate[i].angle == -90){
-      Lcrate[i].body.setSize(32,64);
+    //win condition
+    var win = map.createFromObjects('Objects','winPoint', {key: 'exit'});
+    this.winGroup = this.physics.add.group();
+    this.winGroup.children.iterate(function(child) {
+      child.setImmoveable(true);
+      child.refreshBody();
+    });
+    this.physics.add.collider(this.winGroup, worldLayer, function(s1){
+      var b1 = s1.body;
+      b1.stop();
+    });
+    this.physics.add.collider(this.player, this.winGroup, this.endScene, null, this);
+    for(var i = 0; i < win.length; i++){
+      this.winGroup.add(win[i]);
     }
-  }
 
-  //enemy attributes
-  var enemy = map.createFromObjects('Objects','enemyPoint', {key: 'cook'});
-  this.enemyGroup = this.physics.add.group();
-  this.enemyGroup.children.iterate(function(child) {
-    child.setImmoveable(false);
-    child.refreshBody();
-  });
-  this.physics.add.collider(this.enemyGroup, worldLayer, function(s1){
-    var b1 = s1.body;
-    b1.stop();
-  });
-  this.physics.add.collider(this.player, this.enemyGroup, this.gameOver,null, this);
-  this.physics.add.collider(this.enemyGroup,this.crateGroup);
-  this.physics.add.collider(this.enemyGroup,this.LcrateGroup);
-  this.physics.add.collider(this.enemyGroup);
+    //enemy attributes
+    var enemy = map.createFromObjects('Objects','enemyPoint', {key: 'cook'});
+    this.enemyGroup = this.physics.add.group();
+    this.enemyGroup.children.iterate(function(child) {
+      child.setImmoveable(false);
+      child.refreshBody();
+    });
+    this.physics.add.collider(this.enemyGroup, worldLayer, function(s1){
+      var b1 = s1.body;
+      b1.stop();
+    });
+    this.physics.add.collider(this.player, this.enemyGroup, this.gameOver,null, this);
+    this.physics.add.collider(this.enemyGroup,this.crateGroup);
+    this.physics.add.collider(this.enemyGroup,this.LcrateGroup);
+    this.physics.add.collider(this.enemyGroup);
 
-  for(var i = 0; i < enemy.length; i++){
-    this.enemyGroup.add(enemy[i]);
-    enemy[i]
-    .body
-    .CollideWorldBounds = true;
-    enemy[i]
-    .body.bounce.set(0.1);
-    enemy[i]
-    .body.setDrag(100);
-    enemy[i]
-    .body.setSize(32,32,32,32);
-    enemy[i]
-    .body.width = 32;
+    for(var i = 0; i < enemy.length; i++){
+      this.enemyGroup.add(enemy[i]);
+      enemy[i]
+      .body
+      .CollideWorldBounds = true;
+      enemy[i]
+      .body.bounce.set(0.1);
+      enemy[i]
+      .body.setDrag(100);
+      enemy[i]
+      .body.setSize(32,32,32,32);
+      enemy[i]
+      .body.width = 32;
 
-  }
-  //waterspill attributes
-  var spill = map.createFromObjects('Objects','spillPoint', {key: 'spill'});
-  this.spillGroup = this.physics.add.group();
-  this.spillGroup.children.iterate(function(child) {
-    child.setImmoveable(true);
-    child.refreshBody();
-  });
-  this.physics.add.overlap(this.player, this.spillGroup, function(s1,s2){
-    var x = this.displace()
-    var y = this.displace()
-    this.tweens.add({
-    targets: s1,
-    x: s2.x + x,
-    y: s2.y + y,
-    ease: "Elastic",
-    duration: 1000
-  });
-}, null, this);
+    }
+    //waterspill attributes
+    var spill = map.createFromObjects('Objects','spillPoint', {key: 'spill'});
+    this.spillGroup = this.physics.add.group();
+    this.spillGroup.children.iterate(function(child) {
+      child.setImmoveable(true);
+      child.refreshBody();
+    });
+    this.physics.add.overlap(this.player, this.spillGroup, function(s1,s2){
+      var x = this.displace()
+      var y = this.displace()
+      this.tweens.add({
+      targets: s1,
+      x: s2.x + x,
+      y: s2.y + y,
+      ease: "Elastic",
+      duration: 1000
+    });
+  }, null, this);
 
-  this.physics.add.collider(this.spillGroup, worldLayer);
-  this.physics.add.overlap(this.enemyGroup, this.spillGroup, function(s1,s2){
-    var x = this.displace()
-    var y = this.displace()
-    this.tweens.add({
-    targets: s1,
-    x: s2.x + x,
-    y: s2.y + y,
-    ease: "Elastic",
-    duration: 1000
-  });
-}, null, this);
+    this.physics.add.collider(this.spillGroup, worldLayer);
+    this.physics.add.overlap(this.enemyGroup, this.spillGroup, function(s1,s2){
+      var x = this.displace()
+      var y = this.displace()
+      this.tweens.add({
+      targets: s1,
+      x: s2.x + x,
+      y: s2.y + y,
+      ease: "Elastic",
+      duration: 1000
+    });
+  }, null, this);
 
 
-  for (var i = 0; i < spill.length; i++){
-    this.spillGroup.add(spill[i]);
-    spill[i]
-    .body
-    .CollideWorldBounds = true;
-    spill[i]
-    .body
-    .setMaxVelocity(0);
-    spill[i]
-    .body.setSize(16,16,32,32);
-    spill[i]
-    .body.width = 32;
-  }
-
-  //crack attributes
-  var crack = map.createFromObjects('Objects','crackPoint', {key: 'crack'});
-  this.crackGroup = this.physics.add.group();
-  this.crackGroup.children.iterate(function(child) {
-    child.setImmoveable(true);
-    child.refreshBody();
-  });
-  this.physics.add.overlap(this.player, this.crackGroup, this.crack, null, this);
-  this.physics.add.collider(this.crackGroup, worldLayer);
-  for (var i = 0; i < crack.length; i++){
-    this.crackGroup.add(crack[i]);
-    crack[i]
-    .body
-    .CollideWorldBounds = true;
-    crack[i]
-    .body
-    .setMaxVelocity(0);
-    crack[i]
-    .body.setSize(16,16,32,32);
-    crack[i]
-    .body.width = 32;
-  }
-  //NPC attributes
-  var NPC = map.createFromObjects('Objects','NPCPoint', {key: "onion"});
-  this.NPCGroup = this.physics.add.group();
-  this.physics.add.collider(this.NPCGroup, worldLayer);
-  this.physics.add.collider(this.player, this.NPCGroup);
-  this.physics.add.collider(this.NPCGroup,this.crateGroup);
-  this.physics.add.collider(this.NPCGroup,this.LcrateGroup);
-  this.physics.add.collider(this.NPCGroup);
-  this.physics.add.collider(this.NPCGroup, this.crackGroup, this.gameOver, null, this);
-
-  for(var i = 0; i < NPC.length; i++){
-    this.NPCGroup.add(NPC[i]);
-    NPC[i]
-    .body
-    .CollideWorldBounds = true;
-    NPC[i]
-    .body.bounce.set(0.1);
-    NPC[i]
-    .body.setDrag(100);
-    var ran = Math.random() < 0.6 ? "onion" : "tomato";
-    NPC[i].setTexture(ran);
-
-  }
-
+    for (var i = 0; i < spill.length; i++){
+      this.spillGroup.add(spill[i]);
+      spill[i]
+      .body
+      .CollideWorldBounds = true;
+      spill[i]
+      .body
+      .setMaxVelocity(0);
+      spill[i]
+      .body.setSize(16,16,32,32);
+      spill[i]
+      .body.width = 32;
+    }
   }
 
   update (time, delta) {
@@ -341,7 +234,7 @@ export default class Scene0 extends Phaser.Scene {
       this.music.stop();
       this.scene.start('GameOverScene');
     }
-    const speed = 60;
+    const speed = 80;
     const prevVelocity = this.player.body.velocity.clone();
     // Stop any previous movement from the last frame
     if (this.cursors.left.isUp && this.cursors.right.isUp && this.cursors.up.isUp && this.cursors.down.isUp){
@@ -409,10 +302,6 @@ export default class Scene0 extends Phaser.Scene {
     var plusOrMinus = Math.random() < 0.6 ? -1 : 1;
     //console.log(int)
     return (int * plusOrMinus);
-  }
-
-  crack(player, crack){
-    this.gameLose = true;
   }
 
   }
