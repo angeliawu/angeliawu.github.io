1e8ed819c740dc1ae0629bb0a962575134dc9b1f
jat4625 <jatorr98@gmail.com>
2019-10-14 10:34:51 -0500

    added gameover images

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ class Game extends Phaser.Game {
     this.scene.add('WinScene',WinScene);
     this.scene.add('tutorial', Tutorial)
 
-    this.scene.start('Boot');
+    this.scene.start('GameOverScene');
   }
 }
 
--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -8,13 +8,16 @@ export default class GameOverScene extends Phaser.Scene {
 
   init (data) {
     // Initialization code goes here
-    this.Source = data.scene;
+    //this.Source = data.scene;
   }
 
   preload(){
     // Preload assets
     this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
     this.load.audio('End','./assets/sounds/End.wav')
+    this.load.image('tomato','./assets/fullSized/tomato.png')
+    this.load.image('fries','./assets/fullSized/Fries.png')
+    this.load.image('onion','./assets/fullSized/onion.png')
     //Declare variables for center of the scene
     this.centerX = this.cameras.main.width/2;
     this.centerY=this.cameras.main.height/2;
@@ -30,6 +33,9 @@ export default class GameOverScene extends Phaser.Scene {
       volume:.3,
       loop:true
     });
+    this.add.sprite(700, 500,'onion')
+    this.add.sprite(400, 450,'fries').setScale(0.6)
+    this.add.sprite(200, 500,'tomato').setScale(0.7)
     //Create the scenes
     WebFont.load({
       google:{
@@ -44,6 +50,7 @@ export default class GameOverScene extends Phaser.Scene {
     this.spellOutText(75,75,550,text,100,10, '#fff','Candal');
     this.spellOutText(200,300,550,text2,30,10,'#fff','Modak');
   }
+
 
   spellOutText(x, y, width, text, fontSize, speed, fill, font){
     var sentence = this.add.text(x,y, "", {
