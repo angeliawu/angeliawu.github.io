eccf769364c2104491c3fdd0e71a4b97558bd759
Angelia Wu <42920321+angeliawu@users.noreply.github.com>
2019-10-14 13:09:49 -0500

    title art

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -5,13 +5,41 @@ export default class BootScene extends Phaser.Scene {
     super("Boot");
   }
 
-  preload(){
+  preload()
+  {
     // Preload assets
     this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
+<<<<<<< Updated upstream
     this.load.audio('theme','./assets/sounds/InGame.wav')
+=======
+    this.load.audio('theme','./assets/sounds/InGame.wav');
+    this.load.spritesheet("playbuttons","./assets/fullSized/playButtons.png",
+    {
+      frameHeight: 165,
+      frameWidth: 388
+    });
+    this.load.spritesheet("tutorialbuttons","./assets/fullSized/TutButtons.png",
+    {
+      frameHeight: 158,
+      frameWidth: 387
+    });
+    this.load.spritesheet("optionsbuttons","./assets/fullSized/OptionsButtons.png",
+    {
+      frameHeight: 158,
+      frameWidth: 383
+    });
+    this.load.image("logo", "./assets/fullSized/Title Text.png");
+    this.load.spritesheet('Potato', "./assets/resized/pot32.png",
+    {
+      frameHeight: 32,
+      frameWidth: 22
+    });
+    this.load.image("Cook", "./assets/fullSized/cook.png");
+
+>>>>>>> Stashed changes
     //Declare variables for center of the scene
     this.centerX = this.cameras.main.width/2;
-    this.centerY=this.cameras.main.height/2;
+    this.centerY= this.cameras.main.height/2;
   }
 
   create() {
@@ -31,7 +59,75 @@ export default class BootScene extends Phaser.Scene {
       }
     });
 
+<<<<<<< Updated upstream
     this.cameras.main.setBackgroundColor(0xb79268)
+=======
+    //Background
+    this.cameras.main.setBackgroundColor(0xb6cc9f);
+
+    //Title
+    this.add.sprite(425, 110, 'logo').setScale(0.4);
+
+    //Add sprites
+    this.add.sprite(150, 400, "Potato").setScale(5);
+    this.add.sprite(650, 400, "Cook").setScale(0.25);
+
+    //Play button
+    var b1 = this.add.sprite(this.centerX, 300, 'playbuttons', 0).setScale(0.6).setInteractive();
+    b1.on("pointerover", function()
+    {
+      this.setFrame(1);
+    });
+    b1.on("pointerout", function()
+    {
+      this.setFrame(0);
+    });
+    b1.on("pointerup", function()
+    {
+      this.music.stop();
+      this.scene.start('level1');
+    }, this
+  );
+
+  //Tutorial button
+  var b2 = this.add.sprite(this.centerX, 400, 'tutorialbuttons', 2).setScale(0.6).setInteractive();
+  b2.on("pointerover", function()
+  {
+    this.setFrame(1);
+
+  });
+  b2.on("pointerout", function()
+  {
+    this.setFrame(0);
+  });
+  b2.on("pointerup", function()
+  {
+    this.music.stop();
+    this.scene.start('tutorial');
+  }, this
+);
+
+//Options button
+var b3 =this.add.sprite(this.centerX, 500, 'optionsbuttons', 4).setScale(0.6).setInteractive();
+b3.on("pointerover", function()
+{
+  this.setFrame(1);
+
+});
+b3.on("pointerout", function()
+{
+  this.setFrame(0);
+});
+b3.on("pointerup", function()
+{
+
+  //this.scene.start('Scene2');
+  console.log("to be implemented")
+}, this
+);
+
+
+>>>>>>> Stashed changes
 
     var text = 'Fried or Flight'
     var text2 = 'Use the arrows to move, push crates, and avoid the cooks'
