8e951125680bc97bfb4f7535513ca34b51438ad8
jat4625 <jatorr98@gmail.com>
2019-10-20 22:23:47 -0500

    corrected index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class Game extends Phaser.Game {
     this.scene.add('tutorial', Tutorial)
     this.scene.add('Options', Options)
 
-    this.scene.start('level3');
+    this.scene.start('Boot');
   }
 }
 
